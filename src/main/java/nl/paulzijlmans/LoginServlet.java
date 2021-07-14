d61708045ea844f1c54025cfcd04fd16ccb3156c
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-14 20:39:48 +0200

    Web Applications with Spring MVC - added username and password check

--- src/main/java/nl/paulzijlmans/LoginServlet.java
+++ src/main/java/nl/paulzijlmans/LoginServlet.java
@@ -10,13 +10,23 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
+    private final UserValidationService service = new UserValidationService();
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        request.setAttribute("name", request.getParameter("name"));
-        request.setAttribute("password", request.getParameter("password"));
-        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        boolean isUserValid = service.isUserValid(name, password);
+        if(isUserValid) {
+            request.setAttribute("name", name);
+            request.setAttribute("password", password);
+            request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+        }
     }
 }
